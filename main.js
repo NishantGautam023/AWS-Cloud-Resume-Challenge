bbc596f2bee9ad3bbe30255060d51f76d64e4be1
nishant <nishant.gautam023@outlook.com>
2023-08-02 12:06:33 -0400

    Added Tab Section for Blog

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 if (prefersLight) {
     document.documentElement.classList.add("light-theme");
 }
+
+const tabs = document.querySelectorAll(".tabs .tab");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (event) => {
+        // remove highlight from previous active tab
+        document.querySelector(".tab.active").classList.remove("active");
+        // highlight current tab
+        event.currentTarget.classList.add("active");
+
+        // hide the previously visible tab content
+        document.querySelector(".tab-content.show").classList.remove("show");
+
+        // get the selector from data-content
+        const selector = event.currentTarget.dataset.content;
+        // find the related content and show it
+        document.querySelector(selector).classList.add("show");
+    });
+});
